4d1938b525722c2cfd5b44d5b179eec96f2304b8
ayush111222 <ayusp38@gmail.com>
2022-10-17 06:03:07 +0530

    made some chenges

--- main.js
+++ main.js
@@ -1,3 +1,54 @@
+const unReadMessages = document.querySelectorAll(".box-colour");
+const unread = document.getElementById("notification");
+const markAll = document.getElementById("mark_all");
+
+unread.innerHTML = unReadMessages.length;
+
+unReadMessages.forEach((message) => {
+    message.addEventListener("click", () => {
+           message.classList.remove("box-colour");
+           const newUnreadMessages = document.querySelectorAll(".box-colour");
+           unread.innerHTML = newUnreadMessages.length;
+
+  })
+})
+
+markAll.addEventListener("click", () => {
+    unReadMessages.forEach((message) => {
+      message.classList.remove("box-colour");
+
+    })
+    const newUnreadMessages = document.querySelectorAll(".box-colour");
+    unread.innerHTML = newUnreadMessages.length;
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 var acc = document.getElementsByClassName("box-heading");
 var i;
 
